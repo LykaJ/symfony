62f42e53d336509a121e57f9c033e7ce98a6d6a2
LykaJ <44438883+LykaJ@users.noreply.github.com>
2019-01-18 16:39:08 +0100

    Update Manager.php

--- models/Manager.php
+++ models/Manager.php
@@ -5,7 +5,7 @@ class Manager
 
   protected function dbConnect()
   {
-    $db = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'admin', 'admin32');
+    $db = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', '', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     return $db;
