ade7ef7ea5807df8d0c43a82207c007c21539bda
Alicia Raulet <webdesigner.form@gmail.com>
2019-02-08 10:32:04 +0100

    DB access modified

---
+++ core/database/AccessDb.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Blog;
+
+class AccessDb extends Database
+{
+   public static $host = 'localhost';
+   public static $user = 'root';
+   public static $pass = 'admin32';
+   public static $dbName = 'blog';
+
+}
+
--- core/database/Database.php
+++ core/database/Database.php
@@ -1,23 +1,28 @@
 <?php
 namespace Blog;
 
+require_once('core/database/AccessDb.php');
+
 class Database
 {
     private static $PDOInstance = null;
 
     const DEFAULT_SQL_USER = 'root';
-    const DEFAULT_SQL_HOST = 'localhost';
+   // const DEFAULT_SQL_HOST = 'localhost';
     const DEFAULT_SQL_PASS = 'admin32';
     const DEFAULT_SQL_DTB = 'blog';
+
+
 
     public static function get()
     {
         if (is_null(self::$PDOInstance)) {
             self::$PDOInstance = new \PDO(
-              'mysql:dbname='.self::DEFAULT_SQL_DTB.';host='.self::DEFAULT_SQL_HOST,
-              self::DEFAULT_SQL_USER,
-              self::DEFAULT_SQL_PASS
+              'mysql:dbname='.AccessDb::$dbName.';host='.AccessDb::$host,
+              AccessDb::$user,
+              AccessDb::$pass
           );
+
             self::$PDOInstance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         }
 
