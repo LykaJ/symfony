52b2019a7807e6550a480b0b1e5162a8e72bb193
Alicia Raulet <webdesigner.form@gmail.com>
2019-02-08 10:56:52 +0100

    changes in code

--- index.php
+++ index.php
@@ -17,43 +17,31 @@ if (isset($_GET['url'])) {
 } else {
     $url = "";
 }
-
 $router = new Router($url);
-
 //$router->get('/', 'Home');
-
 $router->get('', '\Blog\controllers\Home#index');
 $router->get('posts', '\Blog\controllers\Posts#index');
 $router->get('posts/new', '\Blog\controllers\Posts#new');
 $router->get('posts/:id', '\Blog\controllers\Posts#show');
 $router->get('admin/validation', '\Blog\controllers\Admin#showUnvalidated');
-
 $router->post('posts/create', '\Blog\controllers\Posts#create');
-
 $router->get('validate/post/:id', '\Blog\controllers\Admin#validatePost');
-
 $router->get('posts/edit/:id', '\Blog\controllers\Posts#edit');
 $router->post('posts/update/:id', '\Blog\controllers\Posts#update');
 $router->get('posts/delete/:id', '\Blog\controllers\Posts#delete');
-
 $router->get('signup', '\Blog\controllers\Users#new');
 $router->post('signup', '\Blog\controllers\Users#create');
 $router->get('signin', '\Blog\controllers\Users#loginForm');
 $router->post('login', '\Blog\controllers\Users#login');
 $router->get('logout', '\Blog\controllers\Session#logout');
-
 $router->get('delete/:id', '\Blog\controllers\Users#delete');
-
 $router->get('validate/user/:id/:profileId', '\Blog\controllers\Admin#validateUser');
-
 $router->post('comments/add/:postId', '\Blog\controllers\Comments#add');
 $router->get('comments/validate/:id/:postId', '\Blog\controllers\Comments#validate');
 $router->get('comments/edit', '\Blog\controllers\Comments#edit');
 $router->post('comments/update', '\Blog\controllers\Comments#update');
 $router->get('comments/delete/:id/:postId', '\Blog\controllers\Comments#delete');
-
 $router->get('contact', '\Blog\controllers\Home#contactForm');
-$router->post('contact/sent', '\Blog\controllers\Home#contactMail');
-
+$router->post('contact', '\Blog\controllers\Home#contactMail');
 
 $router->run();
--- view/frontend/contact.php
+++ view/frontend/contact.php
@@ -2,60 +2,61 @@
 
 <?php ob_start(); ?>
 
-<div class="container">
-    <h1 class="rewrite-bt-banner">Contact</h1>
+    <div class="container">
+        <h1 class="rewrite-bt-banner">Contact</h1>
 
-    <p>Vous avez une question ? Envoyez moi un message :)</p>
+        <p>Vous avez une question ? Envoyez moi un message :)</p>
 
-    <form method="post" action="/Blog/contact/sent">
-        <div class="form-group">
-            <?php if (isset($_SESSION['current_user'])) {
-    ?>
-                <label for="identifiant">Votre Identifiant : <?php echo $_SESSION['current_user']['pseudo']; ?></label>
+        <form method="post" action="/Blog/contact">
+            <div class="form-group">
+                <?php if (isset($_SESSION['current_user'])) {
+                    ?>
+                    <label for="identifiant">Votre Identifiant : <?php echo $_SESSION['current_user']['pseudo']; ?></label>
+                    <?php
+                } else {
+                    ?>
+                    <label for="identifiant">Votre Identifiant*</label>
+                    <input id="pseudo" type="text" name="pseudo" class="form-control" placeholder="Identifiant">
+                    <?php
+                } ?>
+            </div>
+            <div class="form-group">
+                <?php if (isset($_SESSION['current_user'])) {
+                    ?>
+                    <label for="email">Votre Email : <?php echo $_SESSION['current_user']['email']; ?></label>
+                    <?php
+                } else {
+                    ?>
+                    <label for="email">Votre Email*</label>
+                    <input type="email" class="form-control" id="email" placeholder="Saisir email" name="email">
+                    <?php
+                } ?>
+            </div>
+            <div class="form-group">
+                <label for="content">Votre Message*</label>
+                <textarea class="form-control" id="content" rows="3" name="content"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-info">Envoyer</button>
+        </form>
+        <div class="bt-alert">
             <?php
-} else {
-        ?>
-                <label for="identifiant">Votre Identifiant*</label>
-                <input id="pseudo" type="text" name="pseudo" class="form-control" placeholder="Identifiant">
-            <?php
-    } ?>
+            $error = \Blog\get_flash('error');
+            if (!empty($error)) {
+                ?>
+                <div class="alert alert-danger" role="alert"><?= $error ?></div>
+
+                <?php
+            }
+            $success = \Blog\get_flash('success');
+            if (!empty($success)) {
+                ?>
+                <div class="alert alert-success" role="alert"><?= $success ?></div>
+
+                <?php
+            } ?>
         </div>
-        <div class="form-group">
-            <?php if (isset($_SESSION['current_user'])) {
-        ?>
-                <label for="email">Votre Email : <?php echo $_SESSION['current_user']['email']; ?></label>
-            <?php
-    } else {
-        ?>
-                <label for="identifiant">Votre Email*</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Saisir email" name="email">
-            <?php
-    } ?>
-        </div>
-        <div class="form-group">
-            <label for="message">Votre Message*</label>
-            <textarea class="form-control" id="message" rows="3"></textarea>
-        </div>
-
-        <button type="submit" class="btn btn-info">Envoyer</button>
-    </form>
-
-    <?php
-    $error = \Blog\get_flash('error');
-    if (!empty($error)) {
-        ?>
-        <div class="alert alert-danger" role="alert"><?= $error ?></div>
-
-    <?php
-    }
-    $success = \Blog\get_flash('success');
-    if (!empty($success)) {
-        ?>
-        <div class="alert alert-success" role="alert"><?= $success ?></div>
-
-    <?php
-    } ?>
-</div>
+    </div>
 
 <?php $content = ob_get_clean(); ?>
 
