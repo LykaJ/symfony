6106edce6ede762433acc29bfe8be19e80d76a29
Alicia Raulet <webdesigner.form@gmail.com>
2019-02-27 17:22:58 +0100

    Unicity on trick name

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TrickRepository")
+ * @UniqueEntity("title")
  */
 class Trick
 {
