38278926df25a3fb575e7649b237200fcfbe9059
Alicia Raulet <webdesigner.form@gmail.com>
2019-03-21 17:34:20 +0100

    fixtures added

---
+++ src/DataFixtures/CategroryFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CategroryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $category = new Category();
+        $category->setName('grab');
+        $category->setName('flip');
+        $manager->persist($category);
+        $manager->flush();
+    }
+}
