a5a57af875011261d8b700189f917db7e26320cc
Alicia Raulet <webdesigner.form@gmail.com>
2019-04-14 17:25:46 +0200

    all phpunit for entities done

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -9,7 +9,97 @@
 namespace App\Tests\Entity;
 
 
-class UserTest
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class UserTest extends TestCase
 {
 
+    public function testUsername()
+    {
+        $user = new User();
+        $user->setUsername('Jane');
+
+        $this->assertSame('Jane', $user->getUsername());
+    }
+
+    public function testRealName()
+    {
+        $user = new User();
+        $user->setRealName('Jane Doe');
+
+        $this->assertSame('Jane Doe', $user->getRealName());
+    }
+
+    public function testEmail()
+    {
+        $user = new User();
+        $user->setEmail('email@email.com');
+
+        $this->assertSame('email@email.com', $user->getEmail());
+    }
+
+    public function testPassword()
+    {
+        $user = new User();
+        $user->setPassword('Pass');
+
+        $this->assertSame('Pass', $user->getPassword());
+    }
+
+    public function testSignUpDate()
+    {
+        $user = new User();
+        $date = new \DateTime('2019-03-11 17:14:15');
+
+        $user->setSignupDate($date);
+
+        $this->assertSame($date, $user->getSignupDate());
+    }
+
+    public function testLastLogin()
+    {
+        $user = new User();
+        $date = new \DateTime('2019-03-11 17:14:15');
+
+        $user->setLastLogin($date);
+
+        $this->assertSame($date, $user->getLastLogin());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testUploadedPicture()
+    {
+        $user = new User();
+        $path = 'public/uploads/media/3fca48f2b09cd4d8290d23842792d3aa.jpeg';
+        $originalName = 'Toto.jpg';
+        $file = new UploadedFile($path, $originalName);
+        $user->setPictureUpload($file);
+
+        $this->assertSame($file, $user->getPictureUpload());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testPicture()
+    {
+        $user = new User();
+        $file = 'public/uploads/media/3fca48f2b09cd4d8290d23842792d3aa.jpeg';
+        $user->setPicture($file);
+
+        $this->assertSame($file, $user->getPicture());
+    }
+
+    public function testToken()
+    {
+        $user = new User();
+        $token = 'Token';
+        $user->setResetToken($token);
+
+        $this->assertSame($token, $user->getResetToken());
+    }
 }
--- tests/Entity/VideoMediaTest.php
+++ tests/Entity/VideoMediaTest.php
@@ -9,7 +9,28 @@
 namespace App\Tests\Entity;
 
 
-class VideoMediaTest
+use App\Entity\Trick;
+use App\Entity\VideoMedia;
+use PHPUnit\Framework\TestCase;
+
+class VideoMediaTest extends TestCase
 {
 
+    public function testPath()
+    {
+        $videoMedia = new VideoMedia();
+        $path = 'path-to-video';
+        $videoMedia->setPath($path);
+
+        $this->assertSame($path, $videoMedia->getPath());
+    }
+
+    public function testTrick()
+    {
+        $videoMedia = new VideoMedia();
+        $trick = new Trick();
+        $videoMedia->setTrick($trick);
+
+        $this->assertSame($trick, $videoMedia->getTrick());
+    }
 }
