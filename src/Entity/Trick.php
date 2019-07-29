0f7a2136bf9aa3ae056abb8a68712a4b10136298
Alicia Raulet <webdesigner.form@gmail.com>
2019-07-29 10:29:14 +0200

    fixes for trick display

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -6,7 +6,6 @@ use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Cocur\Slugify\Slugify;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -103,11 +102,6 @@ class Trick
     {
         $this->title = $title;
         return $this;
-    }
-
-    public function getSlug(): ?string
-    {
-        return (new Slugify())->slugify($this->title);
     }
 
     public function getContent(): ?string
